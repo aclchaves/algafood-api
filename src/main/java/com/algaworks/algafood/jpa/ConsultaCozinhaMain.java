d5dbf4b59b6aa51f55b176a5f203f0d018b50dce
Andre Chaves <aclchaves@gmail.com>
2022-01-21 16:12:29 -0300

    Adicionando um objeto no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
@@ -19,11 +19,17 @@ public class ConsultaCozinhaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Brasileira");
 		
-		for (Cozinha cozinha : cozinhas) {
-			System.out.println(cozinha.getNome());
-		}
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Japonesa");
+		
+		cozinha1 = cadastroCozinha.adicionar(cozinha1);
+		cozinha2 = cadastroCozinha.adicionar(cozinha2);
+		
+		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
 		
 	}
 
