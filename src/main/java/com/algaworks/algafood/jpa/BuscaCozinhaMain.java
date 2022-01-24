27ee5b01ba12298edda0e1dfa896a4d7930696c5
Andre Chaves <aclchaves@gmail.com>
2022-01-24 11:06:33 -0300

    correção de bugs do ultimo commit

--- src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -1,8 +1,6 @@
 package com.algaworks.algafood.jpa;
 
 
-import java.util.List;
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -19,17 +17,9 @@ public class BuscaCozinhaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
+		Cozinha cozinha = cadastroCozinha.buscar(1L);
 		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
-		
-		cozinha1 = cadastroCozinha.adicionar(cozinha1);
-		cozinha2 = cadastroCozinha.adicionar(cozinha2);
-		
-		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+		System.out.println(cozinha.getNome());
 		
 	}
 
--- src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
@@ -1,8 +1,6 @@
 package com.algaworks.algafood.jpa;
 
 
-import java.util.List;
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -19,9 +17,17 @@ public class ConsultaCozinhaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
-		Cozinha cozinha = cadastroCozinha.buscar(1L);
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Brasileira");
 		
-		System.out.println(cozinha.getNome());
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Japonesa");
+		
+		cozinha1 = cadastroCozinha.adicionar(cozinha1);
+		cozinha2 = cadastroCozinha.adicionar(cozinha2);
+		
+		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());		
 		
 	}
 
