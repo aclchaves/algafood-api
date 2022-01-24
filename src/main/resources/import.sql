de34317f6f0e6d7db88d9426103b9b533d2b57ee
Andre Chaves <aclchaves@gmail.com>
2022-01-24 18:47:25 -0300

    3.16. Desafio: Lombok e repositório de restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,8 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome, taxa_frete) values ('Restaurante Tai Tai', 5.00);
+insert into restaurante (nome, taxa_frete) values ('Restaurante Tic Tic', 5.30);
+insert into restaurante (nome, taxa_frete) values ('Thai Gourmet', 10);
+insert into restaurante (nome, taxa_frete) values ('Thai Delivery', 9.50);
+insert into restaurante (nome, taxa_frete) values ('Tuk Tuk Comida Indiana', 15);
