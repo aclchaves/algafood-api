a79549be77e6c9379be4607c72d10e537309e6dd
Andre Chaves <aclchaves@gmail.com>
2022-01-24 11:33:25 -0300

    adição da classe inclusaoCozinhaMain e alteração na classe ConsultaCozinhaMain

--- src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.jpa;
 
+import java.util.List;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,26 +10,18 @@ import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
 public class ConsultaCozinhaMain {
-	
+
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
-				.web(WebApplicationType.NONE)
-				.run(args);
-		
+				.web(WebApplicationType.NONE).run(args);
+
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
-		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
-		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Japonesa");
-		
-		cozinha1 = cadastroCozinha.adicionar(cozinha1);
-		cozinha2 = cadastroCozinha.adicionar(cozinha2);
-		
-		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());		
-		
+
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+		for (Cozinha cozinha : cozinhas) {
+			System.out.println(cozinha.getNome());
+		}
 	}
 
 }
