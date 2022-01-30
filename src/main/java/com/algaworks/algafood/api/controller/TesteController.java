d12b017d45e8d5f8c072a9c81dbac4400f372630
Andre Chaves <aclchaves@gmail.com>
2022-01-30 19:59:42 -0300

    5.11. Implementando um repositório SDJ customizado

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -68,6 +68,11 @@ public class TesteController {
 			Long cozinhaId){
 		return restauranteRepository.countByCozinhaId(cozinhaId);
 	}
-
+	
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantesPorNomeFrete(String nome,
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+	}	
 
 }
