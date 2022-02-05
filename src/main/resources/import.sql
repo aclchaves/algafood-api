8c699818977f3d9878051fe54fecb99ca5d5c68e
Andre Chaves Lima <andrelima@iftm.edu.br>
2022-02-05 15:21:25 -0300

    6.9. Desafio: mapeando relacionamentos muitos-para-muitos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,13 +18,11 @@ insert into cidade (id, nome, estado_id) values (7, 'Fortaleza', 3);
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10, 1, utc_timestamp, utc_timestamp, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (2, 'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp);
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (3, 'Tuk Tuk Comida Indiana', 15, 2, utc_timestamp, utc_timestamp);
-
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao)  values (4, 'Restaurante Tai Tai', 5.00, 1, utc_timestamp, utc_timestamp);
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (5, 'Restaurante Tic Tic', 5.30, 2, utc_timestamp, utc_timestamp);
-
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (5, 'Java Steakhouse', 12, 3, utc_timestamp, utc_timestamp);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (6, 'Lanchonete do Tio Sam', 11, 4, utc_timestamp, utc_timestamp);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (7, 'Bar da Maria', 6, 4, utc_timestamp, utc_timestamp);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (6, 'Java Steakhouse', 12, 3, utc_timestamp, utc_timestamp);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (7, 'Lanchonete do Tio Sam', 11, 4, utc_timestamp, utc_timestamp);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (8, 'Bar da Maria', 6, 4, utc_timestamp, utc_timestamp);
 
 
 insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
@@ -35,7 +33,7 @@ insert into forma_pagamento (id, descricao) values (4, 'cheque');
 insert into permissao (id, nome,  descricao) values (1, 'USUARIO', 'acesso de usuario comum');
 insert into permissao (id, nome, descricao) values (2, 'ADMINISTRADOR', 'acesso de administrador do sistema');
 insert into permissao (id, nome, descricao) values (3, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
-insert into permissao (id, nome, descricao) values (5, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+insert into permissao (id, nome, descricao) values (4, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3), (7, 1), (7, 2);
 
