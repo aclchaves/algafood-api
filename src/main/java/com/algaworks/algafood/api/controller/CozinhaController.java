9c79edb921cac0903e29ce51cd968d45c44c3481
Andre Chaves Lima <andrelima@iftm.edu.br>
2022-02-06 14:36:30 -0300

    8.4. Estendendo ResponseStatusException

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -88,16 +88,8 @@ public class CozinhaController {
 
 	@DeleteMapping("/{cozinhaId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long cozinhaId) {
-		
-		try {
+	public void remover(@PathVariable Long cozinhaId) {	
 			cadastroCozinha.excluir(cozinhaId);			
-		}catch (EntidadeNaoEncontradaException e) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-					e.getMessage());
-			//throw new ServerWebInputException(e.getMessage());
-		}
-		
 	}
 
 }
