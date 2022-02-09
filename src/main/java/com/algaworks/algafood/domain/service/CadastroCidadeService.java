9a9417aa8df317e234e83dd0af1af27440232412
André Chaves Lima <andrelima@iftm.edu.br>
2022-02-09 18:20:06 -0300

    8.11. Desafio: lançando exceptions de granularidade fina

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -5,8 +5,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.algafood.domain.exception.CidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
@@ -17,10 +17,7 @@ public class CadastroCidadeService {
 
 	private static final String MSG_CIDADE_EM_USO = 
 			"Cidade de código %d  não pode ser removida, pois está em uso";
-
-	private static final String MSG_CIDADE_NAO_ENCONTRADA = 
-			"Não existe um cadastro de cidade com código %d";
-
+	
 	@Autowired
 	CidadeRepository cidadeRepository;
 	
@@ -48,8 +45,7 @@ public class CadastroCidadeService {
 			
 		} catch (EmptyResultDataAccessException e) {
 			
-			throw new EntidadeNaoEncontradaException(
-					String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId));
+			throw new CidadeNaoEncontradaException(cidadeId);
 			
 		}catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
@@ -59,8 +55,7 @@ public class CadastroCidadeService {
 	
 	public Cidade buscarOuFalhar(Long cidadeId) {
 		return cidadeRepository.findById(cidadeId)
-				.orElseThrow(() -> new EntidadeNaoEncontradaException(
-						String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId)));
+				.orElseThrow(() -> new CidadeNaoEncontradaException(cidadeId));
 		
 	}
 }
