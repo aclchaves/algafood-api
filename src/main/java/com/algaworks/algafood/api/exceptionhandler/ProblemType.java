45be94a0c6fc877dbd891b06a25abeb7080db7b4
André Chaves Lima <andrelima@iftm.edu.br>
2022-02-10 11:50:44 -0300

    8.20. Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade está em uso"),
 	ERRO_NEGOCIO("/ERRO-negocio", "Violação de egra de negócio");
