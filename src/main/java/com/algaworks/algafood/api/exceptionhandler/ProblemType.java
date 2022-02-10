7cc5b48576fd28b94bdaffcde05f47f92e8b1d05
André Chaves Lima <andrelima@iftm.edu.br>
2022-02-10 11:31:02 -0300

    8.19. Desafio: usando o formato de problemas no corpo de respostas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
-	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada");
+	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade está em uso"),
+	ERRO_NEGOCIO("/ERRO-negocio", "Violação de egra de negócio");
+	
 	
 	private String title;
 	private String uri;
