3708bba16bd4a098208afeb9adcc008c0e367f8d
André Chaves Lima <andrelima@iftm.edu.br>
2022-02-14 22:00:46 -0300

    10.3. Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -1,16 +1,51 @@
 package com.algaworks.algafood;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
 @SpringBootTest
-class AlgafoodApiApplicationTests {
-
+class CadastroCozinhaIntegrationTests {
+	
+	@Autowired
+	CadastroCozinhaService cadastroCozinha;
+	
 	@Test
-	void contextLoads() {
-		assertFalse(true);
+	public void testarCadastroCozinhaComSucesso() {
+		//cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");		
+		
+		//ação
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);	
+		
+		//validacao
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+	
+	@Test
+	public void testarCadastroCozinhaSemNome() {
+	   Cozinha novaCozinha = new Cozinha();
+	   novaCozinha.setNome(null);
+	   
+	   ConstraintViolationException erroEsperado =
+	      Assertions.assertThrows(ConstraintViolationException.class, () -> {
+	         cadastroCozinha.salvar(novaCozinha);
+	      });
+	   
+	   assertThat(erroEsperado).isNotNull();
 	}
 
+	
 }
