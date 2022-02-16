3b9902646fc631489e667df644435473a4fed5d3
André Chaves Lima <andrelima@iftm.edu.br>
2022-02-16 16:03:50 -0300

    11.3. Criando classes de mixin para usar as anotações do Jackson

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -28,8 +28,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.algaworks.algafood.core.validation.Groups;
 import com.algaworks.algafood.core.validation.ValorZeroIncluiDescricao;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -54,39 +52,33 @@ public class Restaurante {
 	@NotNull
 	@PositiveOrZero
 	@Column(name = "taxa_frete", nullable = false)
-	private BigDecimal taxaFrete;
+	private BigDecimal taxaFrete;	
 	
-	@JsonIgnoreProperties(value = "nome", allowGetters = true )
 	@Valid
 	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
 	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
-	private Cozinha cozinha;
+	private Cozinha cozinha;	
 	
-	@JsonIgnore
 	@Embedded
-	private Endereco endereco;
+	private Endereco endereco;	
 	
-	@JsonIgnore
 	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataCadastro;
-
-	@JsonIgnore
+	
 	@UpdateTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataAtualizacao;
+	private LocalDateTime dataAtualizacao;	
 	
-	@JsonIgnore
 	//não e recomendavel para relacionamento muitos para muitos usar requisação EAGER
 	@ManyToMany//(fetch = FetchType.EAGER)
 	@JoinTable(name = "restaurante_forma_pagamento",
 			joinColumns = @JoinColumn(name = "restaurante_id"),
 			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-	private List<FormaPagamento> formasPagamento = new ArrayList<>();
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();	
 	
-	@JsonIgnore
 	@OneToMany(mappedBy = "restaurante")
 	private List<Produto> produtos = new ArrayList<>();
 }
