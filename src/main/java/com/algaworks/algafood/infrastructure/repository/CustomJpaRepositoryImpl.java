ce9dfb00ed82e5ab8e7a54d298caa9352e990c9c
André Chaves Lima <andrelima@iftm.edu.br>
2022-02-19 10:18:17 -0300

    12.11. Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -12,25 +12,31 @@ import com.algaworks.algafood.domain.repository.CustomJpaRepository;
 public class CustomJpaRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID>
 implements CustomJpaRepository<T, ID> {
 	
-	private EntityManager manger;
+	private EntityManager manager;
 	
 
 	public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation,
 			EntityManager entityManager) {
 		super(entityInformation, entityManager);	
 		
-		this.manger = entityManager;
+		this.manager = entityManager;
 	}
 
 	@Override
 	public Optional<T> buscarPrimeiro() {
 		var jpql = "from " + getDomainClass().getName();
 		
-		T entity = manger.createQuery(jpql, getDomainClass())
+		T entity = manager.createQuery(jpql, getDomainClass())
 			.setMaxResults(1)
 			.getSingleResult();
 		
 		return Optional.ofNullable(entity);
+	}
+
+	@Override
+	public void detach(T entity) {
+		manager.detach(entity);
+		
 	}
 
 }
