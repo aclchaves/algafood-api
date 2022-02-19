596122c35500118ea6baf2d1778bbcc71b510eb2
André Chaves Lima <andrelima@iftm.edu.br>
2022-02-19 11:29:11 -0300

    12.12. Implementando os endpoints de associação de formas de pagamento em restaurantes

--- src/main/java/com/algaworks/algafood/api/assembler/FormaPagamentoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/FormaPagamentoModelAssembler.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.assembler;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,7 +22,7 @@ public class FormaPagamentoModelAssembler {
 		return modelMapper.map(formaPagamento, FormaPagamentoModel.class);		
 	}
 	
-	public List<FormaPagamentoModel> toCollectionModel(List<FormaPagamento> formaPagamentos){
+	public List<FormaPagamentoModel> toCollectionModel(Collection<FormaPagamento> formaPagamentos){
 		return formaPagamentos.stream()
 				.map(formaPagamento -> toModel(formaPagamento))
 				.collect(Collectors.toList());
