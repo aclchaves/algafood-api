ec87a0776cdf53a1624f9764da24320214f4c390
Andre Chaves Lima <andrelima@iftm.edu.br>
2022-02-25 09:56:27 -0300

    13.1. Fazendo projeção de recursos com @JsonView do Jackson

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,7 @@ import com.algaworks.algafood.api.assembler.RestauranteInputDisassembler;
 import com.algaworks.algafood.api.assembler.RestauranteModelAssembler;
 import com.algaworks.algafood.api.model.RestauranteModel;
 import com.algaworks.algafood.api.model.input.RestauranteInput;
+import com.algaworks.algafood.api.model.view.RestauranteView;
 import com.algaworks.algafood.domain.exception.CidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
@@ -27,6 +30,7 @@ import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradaException
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
 @RequestMapping(value = "/restaurantes")
@@ -43,12 +47,44 @@ public class RestauranteController {
 	
 	@Autowired
 	private RestauranteInputDisassembler restauranteInputDisassembler;
-		
-
+	
+	@JsonView(RestauranteView.Resumo.class)
 	@GetMapping
 	public List<RestauranteModel> listar() {
 		return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
-	}	
+	}
+	
+	@JsonView(RestauranteView.ApenasNome.class)
+	@GetMapping(params = "projecao=apenas-nome")
+	public List<RestauranteModel> listarApenasNome() {
+		return listar();
+	}
+		
+//	@GetMapping
+//	public MappingJacksonValue listar(
+//			@RequestParam(required = false) String projecao) {
+//		List<Restaurante> restaurantes = restauranteRepository.findAll();
+//		
+//		List<RestauranteModel> restaurantesModel = 
+//				restauranteModelAssembler.toCollectionModel(restaurantes);
+//		
+//		MappingJacksonValue pedidosWapper = new MappingJacksonValue(restaurantesModel);
+//		
+//		pedidosWapper.setSerializationView(RestauranteView.Resumo.class);
+//		
+//		if("apenas-nome".equals(projecao)) {
+//			pedidosWapper.setSerializationView(RestauranteView.ApenasNome.class);
+//		}else if("completo".equals(projecao)) {
+//			pedidosWapper.setSerializationView(null);
+//		}		
+//		
+//		return pedidosWapper;
+//	}	
+	
+//	@GetMapping
+//	public List<RestauranteModel> listar() {
+//		return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
+//	}	
 
 	@GetMapping("/{restauranteId}")
 	public RestauranteModel buscar(@PathVariable Long restauranteId) {		
--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -2,29 +2,19 @@ package com.algaworks.algafood.core.jackson;
 
 import org.springframework.stereotype.Component;
 
-import com.algaworks.algafood.api.model.mixin.CidadeMixin;
-import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
-import com.algaworks.algafood.api.model.mixin.GrupoMixin;
-import com.algaworks.algafood.api.model.mixin.ProdutoMixin;
-import com.algaworks.algafood.api.model.mixin.UsuarioMixin;
-import com.algaworks.algafood.domain.model.Cidade;
-import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.model.Grupo;
-import com.algaworks.algafood.domain.model.Produto;
-import com.algaworks.algafood.domain.model.Usuario;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @Component
 public class JacksonMixinModule extends SimpleModule{
 	
 	private static final long serialVersionUID = 1L;
-	
-	public JacksonMixinModule() {		
-		setMixInAnnotation(Cidade.class, CidadeMixin.class);
-		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
-		setMixInAnnotation(Grupo.class, GrupoMixin.class);
-		setMixInAnnotation(Produto.class, ProdutoMixin.class);
-		setMixInAnnotation(Usuario.class, UsuarioMixin.class);
-	}
+//	
+//	public JacksonMixinModule() {		
+//		setMixInAnnotation(Cidade.class, CidadeMixin.class);
+//		setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
+//		setMixInAnnotation(Grupo.class, GrupoMixin.class);
+//		setMixInAnnotation(Produto.class, ProdutoMixin.class);
+//		setMixInAnnotation(Usuario.class, UsuarioMixin.class);
+//	}
 
 }
