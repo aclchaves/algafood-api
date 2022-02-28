a2b6b06c1fd40584d5bb6dd649679d06bfc17ed5
Andre Chaves Lima <andrelima@iftm.edu.br>
2022-02-28 18:58:19 -0300

    13.15. Desafio: adicionando os filtros na consulta de vendas diárias

--- src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,15 +1,18 @@
 package com.algaworks.algafood.infrastructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 
@@ -25,6 +28,26 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 		var query = builder.createQuery(VendaDiaria.class);
 		var root = query.from(Pedido.class);
 		
+		var predicates = new ArrayList<Predicate>();
+		
+		if(filtro.getRestauanteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauanteId()));
+		}
+		
+		if(filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+		}
+		
+		if(filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+		}
+		
+		predicates.add(root.get("status").in(
+				StatusPedido.CONFIRMADO,
+				StatusPedido.ENTREGUE));
+		
+		query.where(predicates.toArray(new Predicate[0]));
+		
 		var functionDateDataCriacao = builder.function(
 				"date",
 				Date.class,
@@ -37,6 +60,10 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 				);
 		query.select(selection);
 		query.groupBy(functionDateDataCriacao);
+		
+		
+		
+		
 		
 		return manager.createQuery(query).getResultList();
 	}
